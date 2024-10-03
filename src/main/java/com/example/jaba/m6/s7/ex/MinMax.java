96e3260afd2022e335681fde259da2617fcee689
Adriana Buccioli <adrianaidabuccioli98@gmail.com>
2024-10-03 12:28:08 +0200

    Esercizio  su  Math

---
+++ src/main/java/com/example/jaba/m6/s7/ex/MinMax.java
@@ -0,0 +1,21 @@
+package com.example.jaba.m6.s7.ex;
+
+/**
+ * Given two values print
+ */
+public class MinMax {
+	public static void main(String[] args) {
+		int x = 42;
+		System.out.println("x is " + x);
+		int y = 53;
+		System.out.println("y is " + y);
+
+		// TODO: put in max the largest between x and y
+		int max = Math.max(x, y);
+		System.out.println("max is " + max);
+		// TODO: put in max the smallest between x and y
+		int min = Math.min(x, y);
+		System.out.println("Min is " + min);
+	}
+
+}
